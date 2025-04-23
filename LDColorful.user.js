d07e0f230c2af1dda89861d2ae17ddac9d5ff2bf
1e0n <voop@qq.com>
2025-04-23 18:51:04 +0800

    Update author name

--- LDColorful.user.js
+++ LDColorful.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Color-code topics on linux.do based on category level (Lv1, Lv2, Lv3) with customizable colors via menu.
-// @author       Cascade via User Request
+// @author       1e0n
 // @match        https://linux.do/
 // @match        https://linux.do
 // @downloadURL  https://raw.githubusercontent.com/1e0n/LinuxDoColorful/main/LDColorful.user.js
